a5d162aa34e13c84dce0cf529c46d614fa3dab37
zeilmatt <zeilmatt@umich.edu>
2018-11-02 04:10:43 -0400

    Added tests for * and /

--- test_rpn.py
+++ test_rpn.py
@@ -5,7 +5,15 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
-    #TODO: Add test for subtraction
+    
     def test_sub(self):
         result = rpn.calculate("4 3 -")
         self.assertEqual(1, result)
+
+    def test_mult(self):
+        result = rpn.calculate("5 6 *")
+        self.assertEqual(30, result)
+
+    def test_div(self):
+        result = rpn.calculate("69 3 /")
+        self.assertEqual(23, result)
